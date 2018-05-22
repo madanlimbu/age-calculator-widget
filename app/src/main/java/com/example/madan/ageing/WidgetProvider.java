8d95dcb3f8e0727d1e57f2088e3f306e894ca30e
Madan <madan_limbu@hotmail.co.uk>
2018-05-22 22:58:53 +0100

    Widget View created and onUpdate template created for more htan one widget if available

---
+++ app/src/main/java/com/example/madan/ageing/WidgetProvider.java
@@ -0,0 +1,39 @@
+package com.example.madan.ageing;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import java.util.Random;
+
+public class WidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+        System.out.println("Widget Provider onUpdate()");
+
+
+        final int count = appWidgetIds.length;
+
+        for (int i = 0; i < count; i++) {
+            int widgetId = appWidgetIds[i];
+            String number = String.format("%03d", (new Random().nextInt(900) + 100));
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.widget_layout);
+            remoteViews.setTextViewText(R.id.age_in_widget, number);
+
+            Intent intent = new Intent(context, WidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.age_in_widget, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+
+    }
+}
